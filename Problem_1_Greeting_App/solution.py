a3fb41cb1a0b558fc2811de2f880170521209a88
2129-coder <kalkidanzenebe94@gmail.com>
2025-10-18 18:18:17 +0300

    Implement greeting and birth year calculation

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -1,1 +1,8 @@
 # your code here
+name = input("Enter your name: ")
+food = input("Enter your favorite food: ")
+current_year = int(input("Enter the current year: "))
+age = int(input("Enter your age: "))
+birth_year = current_year - age
+print("Hello, {name}! It's your favorite  {food}.")
+print("Based on your age, you were likely born in the year {birth_year}.")
