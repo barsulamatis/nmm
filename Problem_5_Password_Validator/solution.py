6a33ef67e98387ce34d9a6091078077b36233b3a
muluneh23 <muluneh819@gmail.com>
2025-10-19 02:15:27 -0700

    completed problem 5

--- Problem_5_Password_Validator/solution.py
+++ Problem_5_Password_Validator/solution.py
@@ -1,1 +1,17 @@
-# your code here
+# take input from user
+name=input("enter your name:").strip()
+password=input("enter the password").strip()
+missing_rules = []
+if len(password)<8:
+    missing_rules.append("password must be at least 8 characters")
+if not any(char.isdigit() for char in password):
+     missing_rules.append("password is at least contain one digit")
+if password.lower()== name.lower():
+     missing_rules.append("password is must not be the same as your name")    
+if not missing_rules:
+    print("Your password is strong ")
+else:
+    print("Your password is weak . Missing rules:")
+    for rule in missing_rules:
+        print("-", rule)
+     
