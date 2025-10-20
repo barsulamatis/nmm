68443925bf390807d839387782867d62dc75b54b
2129-coder <kalkidanzenebe94@gmail.com>
2025-10-20 13:18:42 -0400

    problem 1 Gretting app

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -1,1 +1,10 @@
 # your code here
+name = input("Enter your name: ")
+favorite_food = input("Enter your favorite food: ")
+year = int(input("Enter the current year: "))
+age = int(input("Enter your age: "))
+
+print(f"\nHello {name}! It's great to know that your favorite food is {favorite_food}.")
+
+birth_year = year - age
+print(f"You were born in the year {birth_year}.")
