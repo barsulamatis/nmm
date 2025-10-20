130c580d3b638e256bc3546c8b1aaefe24619ca7
liuldemissie5-lgtm <liuldemissie5@gmail.com>
2025-10-21 01:09:58 +0300

    Done by liul demissie 1700832 2nd year soft class

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -1,5 +1,7 @@
 name = input("enter your name   ")
 favorite = input("favorite food   ")
-year = int(input("year  "))
+year = int(input("current year  "))
 age = int(input("age   "))
-print("Name:", name, "favorite food:", favorite, "year:", year, "age:", age)
+birth_year = year - age
+print("so you were probably born in" ,   birth_year)
+print("Name:", name, ",favorite food:", favorite, ", current year:", year, ",age:", age)
